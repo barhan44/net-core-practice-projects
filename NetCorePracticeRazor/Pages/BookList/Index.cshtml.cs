2d54c1d1f9cce709922c5d6b0a5f8484191b5f70
barhan44 <gunyashov44@gmail.com>
2020-02-09 03:24:33 +0300

    Add: delete functionality

--- NetCorePracticeRazor/Pages/BookList/Index.cshtml.cs
+++ NetCorePracticeRazor/Pages/BookList/Index.cshtml.cs
@@ -24,5 +24,18 @@ namespace NetCorePracticeRazor
         {
             Books = await _db.Book.ToListAsync();
         }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            Book book = await _db.Book.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            _db.Book.Remove(book);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+        }
     }
 }
