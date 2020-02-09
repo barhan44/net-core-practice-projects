bb197f4508800c6d0cd286db53a4acb2f8362982
barhan44 <gunyashov44@gmail.com>
2020-02-09 22:38:53 +0300

    Add project files.

---
+++ NetCorePracticeMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCorePracticeMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
