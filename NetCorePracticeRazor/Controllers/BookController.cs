be487430ede2256460f04a168166bf9c9098c7d2
barhan44 <gunyashov44@gmail.com>
2020-02-09 22:12:42 +0300

    Add: delete for book table

--- NetCorePracticeRazor/Controllers/BookController.cs
+++ NetCorePracticeRazor/Controllers/BookController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using NetCorePracticeRazor.Model;
 
 namespace NetCorePracticeRazor.Controllers
@@ -19,9 +20,22 @@ namespace NetCorePracticeRazor.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            return Json(new { data = _db.Book.ToList() });
+            return Json(new { data = await _db.Book.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Book boodFromDb = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if (boodFromDb == null) 
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _db.Book.Remove(boodFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successful" });
         }
     }
 }
