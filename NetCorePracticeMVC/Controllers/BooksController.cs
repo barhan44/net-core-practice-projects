49961b8dc7547a8cf198f327d7da4bfe2358e639
barhan44 <gunyashov44@gmail.com>
2020-02-09 23:14:22 +0300

    Add: API calls to books controller

--- NetCorePracticeMVC/Controllers/BooksController.cs
+++ NetCorePracticeMVC/Controllers/BooksController.cs
@@ -3,14 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NetCorePracticeMVC.Models;
 
 namespace NetCorePracticeMVC.Controllers
 {
     public class BooksController : Controller
     {
+
+        private readonly ApplicationDbContext _db;
+
+        public BooksController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        #region API Calls
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Books.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Book boodFromDb = await _db.Books.FirstOrDefaultAsync(u => u.Id == id);
+            if (boodFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _db.Books.Remove(boodFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete successful" });
+        }
+        #endregion
     }
 }
